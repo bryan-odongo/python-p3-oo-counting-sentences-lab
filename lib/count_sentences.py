a182795ca8cb2d537f61bbd80d36da8a8945d1a3
odongo <saonarawestinc@outlook.com>
2024-06-10 07:40:21 +0300

    completed counting sentences lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
+import re
+
 
 class MyString:
-  pass
+    def __init__(self, string=""):
+        self.string = string
+
+    @property
+    def value(self):
+        return self._string
+
+    @value.setter
+    def value(self, value):
+        if isinstance(value, str):
+            self._string = value
+
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return True if self.string.endswith(".") else False
+
+    def is_question(self):
+        return True if self.string.endswith("?") else False
+
+    def is_exclamation(self):
+        return True if self.string.endswith("!") else False
+
+    def count_sentences(self):
+        sentences = re.split(r"[.!?]", self.string)
+
+        sentences = [string for string in sentences if string.strip()]
+        return len(sentences)
